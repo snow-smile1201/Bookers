551936755e19da8a3fc1531d9653a9d50ff78b1d
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-09-04 13:34:08 +0000

    [Add]編集機能実装、更新機能実装、showページ作成、show,editリンク作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,9 +15,17 @@ end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   private
