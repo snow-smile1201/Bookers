2b33bdda003cf8fd46fc5e156afa0ead11a6cfc7
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-09-06 02:22:15 +0000

    [Add]テーブル表示実装、エラーメッセージ実装途中、バリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,8 +11,13 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id)
+    else
+      @book = Book.new
+      @books = Book.all
+      render 'books/index'
+    end
   end
 
   def show
